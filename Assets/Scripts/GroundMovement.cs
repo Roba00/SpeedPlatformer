84ab04fc01d2076ef2814b4a79b4a8824e879809
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2018-11-21 17:38:34 -0600

    Initial commit.

---
+++ Assets/Scripts/GroundMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour {
+
+    public Rigidbody groundRigidbody;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        groundRigidbody.AddForce(-transform.right * 100000 * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody playerRigidbody;
+    public bool isGrounded = false;
+    public Vector3 playerVector;
+    public Transform playerTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Game has started running!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (isGrounded)
+        {
+            if (Input.GetKeyDown("up"))
+            {
+                playerRigidbody.AddForce(transform.up * 400);
+            }
+        }
+        if (Input.GetKey("left"))
+        {
+            playerRigidbody.AddForce(-transform.right * 10);
+        }
+        if (Input.GetKey("right"))
+        {
+            playerRigidbody.AddForce(transform.right * 10);
+        }
+        if (Input.GetKey("x"))
+        {
+            playerRigidbody.AddForce(-transform.up * 10);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
